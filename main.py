9f5a15b2a7c2084461194d98d32a979b5d9b93b5
Pirxcy <71592833+PirxcyFinal@users.noreply.github.com>
2022-09-22 18:27:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import os
-os.system("pip install --upgrade pip")
-os.system("pip install Lobbybot")
-
+print('\033[94m[+] Packages Installing')
+os.system("pip install --upgrade pip >/dev/null 2>&1")
+print("[+] Packager Installed")
+os.system("python3 -m poetry add LobbyBot >/dev/null 2>&1")
+print("[+] Packages Installed, Installing With PIP")
+os.system("pip install Lobbybot >/dev/null 2>&1")
+print("[+] All Done, Loading PirxcyBot!")
 
 import LobbyBot
+print("Shit Boutta get real")
 LobbyBot.run(ip="0.0.0.0",port=1942)
